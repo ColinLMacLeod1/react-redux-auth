14e216c300ed236ad9214c6a4784f7e2bbb39731
colinlmacleod1 <colinlmacleod1@gmail.com>
2018-05-21 18:23:54 -0400

    Finished signup redux

--- src/signup/reducer.js
+++ src/signup/reducer.js
@@ -1,4 +1,4 @@
-
+import { SIGNUP_REQUESTING } from './constants'
 
 const initialState = {
   requesting: false,
@@ -6,3 +6,19 @@ const initialState = {
   messages: [],
   errors: [],
 }
+
+const reducer = function signupReducer (state = initialState, action) {
+  switch (action.type) {
+    case SIGNUP_REQUESTING:
+      return {
+        requesting: true,
+        seccessful: false,
+        messages: [{ body: 'Signing up...', time: new Date() }],
+        errors: [],
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
