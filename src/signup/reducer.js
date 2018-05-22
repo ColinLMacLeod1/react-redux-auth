dcabbdc51198d2937c94c3c34625ed8443d08e4c
colinlmacleod1 <colinlmacleod1@gmail.com>
2018-05-22 10:29:54 -0400

    finished signup page

--- src/signup/reducer.js
+++ src/signup/reducer.js
@@ -1,4 +1,8 @@
-import { SIGNUP_REQUESTING } from './constants'
+import {
+  SIGNUP_REQUESTING,
+  SIGNUP_SUCCESS,
+  SIGNUP_ERROR
+} from './constants'
 
 const initialState = {
   requesting: false,
@@ -16,6 +20,26 @@ const reducer = function signupReducer (state = initialState, action) {
         messages: [{ body: 'Signing up...', time: new Date() }],
         errors: [],
       }
+    case SIGNUP_SUCCESS:
+      return {
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: `Successfully created account for ${action.response.email}`,
+          time: new Date()
+        }],
+        errors: [],
+      }
+    case SIGNUP_ERROR:
+    return {
+      requesting: false,
+      successful: false,
+      messages: [],
+      errors: state.errors.concat([{
+        body: action.error.toString(),
+        time: new Date(),
+      }]),
+    }
     default:
       return state
   }
