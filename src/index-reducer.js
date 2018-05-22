686b1db204d8a5b958910338f1dac2d382968a08
colinlmacleod1 <colinlmacleod1@gmail.com>
2018-05-22 19:21:29 -0400

    working on login sagas

--- src/index-reducer.js
+++ src/index-reducer.js
@@ -1,12 +1,14 @@
-import { combineReducers } from 'redux'
-import { reducer as form } from 'redux-form'
-import client from './client/reducer'
-import signup from './signup/reducer'
+import { combineReducers } from "redux"
+import { reducer as form } from "redux-form"
+import client from "./client/reducer"
+import signup from "./signup/reducer"
+import login from "./login/reducer"
 
 const IndexReducer = combineReducers({
-  form,
-  client,
-  signup,
+	form,
+	client,
+	signup,
+	login,
 })
 
 export default IndexReducer
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
-import { BrowserRouter, Route} from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
+import history from './history'
 
 import App from './App'
 import Login from './login'
@@ -18,29 +19,33 @@ import IndexSagas from './index-sagas'
 const sagaMiddleware = createSagaMiddleware()
 
 /*eslint-disable*/
-const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+const composeSetup =
+	process.env.NODE_ENV !== 'production' &&
+	typeof window === 'object' &&
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: compose
 /*eslint-enable*/
 
 const store = createStore(
-  IndexReducer,
-  composeSetup(applyMiddleware(sagaMiddleware)),
+	IndexReducer,
+	composeSetup(applyMiddleware(sagaMiddleware)),
 )
 
 sagaMiddleware.run(IndexSagas)
 
-
-
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/widgets" component={Widgets} />
-      </App>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'));
-registerServiceWorker();
+	<Provider store={store}>
+		<Router history={history}>
+			<App>
+				<Switch>
+					<Route path="/login" component={Login} />
+					<Route path="/signup" component={Signup} />
+					<Route path="/widgets" component={Widgets} />
+				</Switch>
+			</App>
+		</Router>
+	</Provider>,
+	document.getElementById('root'),
+)
+registerServiceWorker()
